61c88d0a553aad1850bc3efb6ccee2b1aba3c85f
sudoarun <arunrajorya1999@gmail.com>
2024-07-19 12:38:33 +0530

    added slider on movies list card

--- app/home/movie/page.js
+++ app/home/movie/page.js
@@ -1,15 +1,29 @@
 "use client";
 import MoviePosterComp from "@/components/moviePage/MoviePosterComp";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 import { ChevronLeft } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Page = () => {
+  const [state, setState] = useState([]);
+  let slider = {
+    drag: "free",
+    gap: "2rem",
+    perPage: 4.5,
+    // arrows: false,
+    pagination: false,
+  };
   const backFunction = () => {
     history.back();
   };
+  useEffect(() => {
+    let count = 56;
+    let array = Array.from(Array(count).keys());
+    setState(array);
+  }, []);
   return (
     <div className="bg-black text-white p-3">
-      <div className="container ">
+      <div className="container pb-5">
         <div className="flex gap-3 items-center">
           <span
             className="transParentBtn p-2 cursor-pointer"
@@ -24,13 +38,16 @@ const Page = () => {
             Back home
           </span>
         </div>
-        <div className="mt-6 ps-4 pb-2">
+        <div className="py-6 ps-4 pb-2">
           <h3 className="font-semibold text-xl">Top Rated Movies</h3>
-          <div className="mt-5 flex gap-8">
-            <MoviePosterComp />
-            <MoviePosterComp />
-            <MoviePosterComp />
-            <MoviePosterComp />
+          <div className="py-5 flex gap-8">
+            <Splide options={slider} className="w-full">
+              {state?.map((el) => (
+                <SplideSlide>
+                  <MoviePosterComp />
+                </SplideSlide>
+              ))}
+            </Splide>
           </div>
         </div>
       </div>
